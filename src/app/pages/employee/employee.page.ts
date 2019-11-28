0e50701600bfc4769c60aaa56aa2c95cd8e62b59
Jclavo <jclavotafur@gmail.com>
2019-11-27 21:25:41 -0300

    adding call to API

--- src/app/pages/employee/employee.page.ts
+++ src/app/pages/employee/employee.page.ts
@@ -5,6 +5,10 @@ import { NgForm } from '@angular/forms';
 import { EmployeeModel } from '../../models/employee.model';
 import { CountryModel } from '../../models/country.model';
 
+//SERVICE
+import { AlertService } from '../../services/alert.service';
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.page.html',
@@ -17,7 +21,8 @@ export class EmployeePage implements OnInit {
 
   private countries: Array<CountryModel> = [];
 
-  constructor() { }
+  constructor(private alertService: AlertService,
+    private authService: AuthService) { }
 
   ngOnInit() {
     this.mockCountries();
@@ -35,6 +40,56 @@ export class EmployeePage implements OnInit {
   save(formEmployee: NgForm)
   {
     console.log('formEmployee: ',formEmployee.value);
+    if(this.employee.employee_id)
+    {
+      this.CRUDEmployee('',formEmployee);
+    }
+    else
+    {
+      this.CRUDEmployee('',formEmployee);
+    }
+    
+  }
+
+  CRUDEmployee(StatusCRUD: string, formEmployee: NgForm)
+  {
+    let params = {
+      'CodigoUsuarioSistema': this.authService.CodigoUsuarioSistema,
+      'StatusCRUD': StatusCRUD,
+      'formValues': (formEmployee) ? formEmployee.value : ""
+
+    };
+
+    // API methode's name
+    // Store Procedure's name
+    // parameters
+
+    this.authService.QueryStoreProc('Executar', 'spCRUDEmployee', params).then(res => {
+      let resultado: any = res[0];
+      try {
+        if (resultado.success) {
+          console.log(resultado.message);
+          if (resultado.results) 
+          {
+            this.employee.employee_id = JSON.parse(resultado.results)[0].employee_id;
+            this.employee.employee_id = JSON.parse(resultado.results)[0].employee_id;
+            this.employee.employee_id = JSON.parse(resultado.results)[0].employee_id;
+            this.employee.employee_id = JSON.parse(resultado.results)[0].employee_id;
+            this.employee.employee_id = JSON.parse(resultado.results)[0].employee_id;
+            this.employee.employee_id = JSON.parse(resultado.results)[0].employee_id;
+            
+          } 
+
+        }
+        else {
+          this.alertService.presentAlert({ pTitle: 'ATENÇÃO', pSubtitle: "this.AppName", pMessage: resultado.message });
+          //this.router.navigate(['/home']);
+        }
+      } catch (err) {
+        this.alertService.presentAlert({ pTitle: 'ATENÇÃO', pSubtitle: "this.AppName", pMessage: resultado.message });
+        //this.router.navigate(['/home']);
+      }
+    });
   }
 
 }
