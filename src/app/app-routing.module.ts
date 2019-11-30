213b37ccf14d3d37047f6fc99ed7332d00802250
Jclavo <jclavotafur@gmail.com>
2019-11-30 15:41:22 -0300

    add page employee-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'menuoptions',
     loadChildren: () => import('./pages/menuoptions/menuoptions.module').then( m => m.MenuoptionsPageModule)
+  },
+  {
+    path: 'employee-list',
+    loadChildren: () => import('./pages/employee-list/employee-list.module').then( m => m.EmployeeListPageModule)
   }
 ];
 
